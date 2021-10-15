e8d6b1f1fff9aad6dc753ecc4ffd5a11b7702bd6
Vonds <vonds@Vondss-Air.cable.rcn.com>
2021-10-15 08:54:48 -0400

    simple_multiplication 2

--- simple_multiplication.py
+++ simple_multiplication.py
@@ -1,5 +1,7 @@
+#  https://www.codewars.com/kata/583710ccaa6717322c000105
 def simple_multiplication(number):
-    return number * 9 if number % 2 else number * 8
+    return number * 9 if number % 2 == 0 else number * 8
 
 print(simple_multiplication(8)) # 64
-print(simple_multiplication(7)) # 56
+print(simple_multiplication(7)) # 63
+
