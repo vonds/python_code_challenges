1aa77ec62b07c5b18ee4860f1dd2dc21cd5e6a71
Vonds <37168302+vonds@users.noreply.github.com>
2021-11-04 10:52:42 -0400

    Add files via upload

---
+++ find_maximum_and_minimum_values_of_a_list.py
@@ -0,0 +1,21 @@
+# https://www.codewars.com/kata/577a98a6ae28071780000989
+
+def minimum(arr):
+    return min(arr)
+
+def maximum(arr):
+    return max(arr)
+
+# def min(arr):
+#     low = arr[0]
+#     for i in arr[1:]:
+#         if i < low:
+#             low = i
+#     return low
+
+# def max(arr):
+#     high = arr[0]
+#     for i in arr[1:]:
+#         if i > high:
+#             high = i
+#     return high
