e10fb939abd0cf1938ee0184127d12d7c8f92a43
Vonds <37168302+vonds@users.noreply.github.com>
2021-11-09 22:12:13 -0500

    Add files via upload

---
+++ grasshopper_summation.py
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/55d24f55d7dd296eb9000030
+
+def summation(num):
+    return sum(range(1, num + 1))
+
+# def summation(num):
+#     total = 0
+#     for n in range(1, num + 1):
+#         total += n
+#     return total
+    
