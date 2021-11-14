138c89cb786964bb6bf6e8847693c0c96e104dbd
Vonds <37168302+vonds@users.noreply.github.com>
2021-11-14 18:23:08 -0500

    Add files via upload

---
+++ jennys_secret_message.py
@@ -0,0 +1,5 @@
+# https://www.codewars.com/kata/55225023e1be1ec8bc000390
+def greet(name):
+    if name == "Johnny":
+        return "Hello, my love!"
+    return "Hello, {name}!".format(name = name)
